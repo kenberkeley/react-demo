942024f9bd6c0b6a23dd19f6f1b5296e282429df
kenberkeley <kenberkeley@foxmail.com>
2016-08-26 18:02:44 +0800

    add comment for UTIL/makeContainer

--- src/utils/makeContainer.js
+++ src/utils/makeContainer.js
@@ -1,5 +1,12 @@
 import { connect } from 'react-redux'
 
+/**
+ * 将木偶组件变成智能组件
+ * @param  {Function} mapStateToProps
+ * @param  {Object}   mapActionCreators
+ * @param  {Component?}
+ * @return {Connect : Container}
+ */
 export default function makeContainer(mapStateToProps, mapActionCreators, component) {
   var connectComponent = connect(mapStateToProps, mapActionCreators)
   return component ? connectComponent(component) : connectComponent
