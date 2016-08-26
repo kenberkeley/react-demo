be5474af104f4aced0a4b6663dc37d105ba3a949
kenberkeley <kenberkeley@foxmail.com>
2016-08-26 15:14:54 +0800

    API port change to 8989

--- src/services/xhr/config.js
+++ src/services/xhr/config.js
@@ -2,7 +2,7 @@
 // 更多配置请根据业务逻辑自行实现
 
 // 后端 API 地址，最好以http(s)://打头
-export const rootPath = 'http://localhost:9000'
+export const rootPath = 'http://localhost:8989'
 
 export const errHandler = (e) => {
   alert('[ XHR:Failed ] 详情请看控制台')
