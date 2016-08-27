23313042343433f7fedcb77cf711ce99292a399c
kenberkeley <kenberkeley@foxmail.com>
2016-08-27 23:30:36 +0800

    git case sensitive

---
+++ src/services/xhr/superagent.js
@@ -0,0 +1,25 @@
+import request from 'superagent'
+import { rootPath, errHandler } from './config'
+
+const xhr = ({ url, body = null, method = 'get' }) => {
+  // P.S: 此处引入了ES6的Promise实现
+  return new Promise((resolve, reject) => {
+    request[method.toLowerCase()](rootPath + url)
+      .send(body)
+      // .withCredentials()
+      .end((err, re) => {
+        if (err)
+          return errHandler(err)
+
+        if (!re.body)
+          return resolve(null)
+
+        if (re.body._code)
+          return errHandler(re.body._msg)
+
+        resolve(re.body)
+      })
+  })
+}
+
+export default xhr
